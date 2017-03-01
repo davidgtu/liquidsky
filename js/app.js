c15f640d9d3e75996a239e6140900022016bb4ec
spacebear5000 <david.giahuy.tu@gmail.com>
2017-03-01 02:06:28 -0500

    add sections

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-  
+  $(window).scroll(function(){
+    // This is then function used to detect if the element is scrolled into view
+    function elementScrolled(elem)
+    {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = $(elem).offset().top;
+        return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
+    // This is where we use the function to detect if ".box2" is scrolled into view, and when it is add the class ".animated" to the <p> child element
+    if(elementScrolled('#diagram')) {
+        var els = $('.box2 p'),
+            i = 0,
+            f = function () {
+                $(els[i++]).addClass('animated fadeInDown');
+                if(i < els.length) setTimeout(f, 400);
+            };
+        f();
+    }
+});
 })
